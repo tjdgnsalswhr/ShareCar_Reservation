1cf3edd5dfd2c01f425819ea57f5a9fa4fd87c33
tjdgnsalswhr <32426312+tjdgnsalswhr@users.noreply.github.com>
2021-09-02 14:20:29 +0900

    Add files via upload

---
+++ Reservation/src/main/java/sharecar/ReservationCanceled.java
@@ -0,0 +1,34 @@
+package sharecar;
+
+public class ReservationCanceled extends AbstractEvent {
+
+    private Long id;
+    private String status;
+    private Long orderId;
+
+    public ReservationCanceled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+}
